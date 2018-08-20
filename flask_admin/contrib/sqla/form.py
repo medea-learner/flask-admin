acf3322f86d53b2d3fbb26c6462ae194a9c2816a
Fouad CHENNOUF <fouad.chennouf@gmail.com>
2018-08-20 17:12:16 +0200

    bug correction of primary / foreign key order

--- flask_admin/contrib/sqla/form.py
+++ flask_admin/contrib/sqla/form.py
@@ -169,9 +169,7 @@ class AdminModelConverter(ModelConverterBase):
 
             form_columns = getattr(self.view, 'form_columns', None) or ()
 
-            # Do not display foreign keys - use relations, except when explicitly instructed
-            if column.foreign_keys and prop.key not in form_columns:
-                return None
+
 
             # Only display "real" columns
             if not isinstance(column, Column):
@@ -195,6 +193,11 @@ class AdminModelConverter(ModelConverterBase):
                                                            model,
                                                            column))
                         unique = True
+
+            # Do not display foreign keys - use relations, except when explicitly instructed
+            # In case of primary key/ foreignkey used in inhertiance,
+            if column.foreign_keys and prop.key not in form_columns:
+                return None
 
             # If field is unique, validate it
             if column.unique and not unique:
